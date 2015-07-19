40a70016362a6749f58c3434a2f7392dd3086f93
tangalan <alan.tangsh@gmail.com>
2015-07-19 01:02:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/alantang/lunchbuddy/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.alantang.lunchbuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "log_message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        sets content to be activity_main.xml
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
